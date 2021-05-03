131fe59a623345efb69b194a4bfc41ba23e5c6d2
matangro <58812754+matangro@users.noreply.github.com>
2021-05-03 14:34:38 +0300

    Update names files, make consent index as first page

--- MemoryGame/App_Start/RouteConfig.cs
+++ MemoryGame/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MemoryGame
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "ConsentIndex", id = UrlParameter.Optional }
             );
         }
     }
