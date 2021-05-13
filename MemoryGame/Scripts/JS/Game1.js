9787564bb023ec78c5cbbaf9879e91589dfcf15a
itay198 <itay_h@outlook.co.il>
2021-05-13 17:48:30 +0300

    update game js

--- MemoryGame/Scripts/JS/Game1.js
+++ MemoryGame/Scripts/JS/Game1.js
@@ -8,23 +8,48 @@ var choicesIndexes = {};
 var turns = [];
 var remainingCards;
 var size;
-var data = {
-    overallTime: "",// times in milliseconds
-    personalTime: 8000,
-    numOfCards: 4,
-    numOfAgents: 2
-};
+var currentPlayer = 0;
+var noa;
+var globalTime = new Date(0);
+var lockClicks;
 var crads_dict = {};
 crads_dict["cow"] = 'https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg';
 crads_dict["dog"] = 'https://i.guim.co.uk/img/media/20098ae982d6b3ba4d70ede3ef9b8f79ab1205ce/0_0_969_581/master/969.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=a368f449b1cc1f37412c07a1bd901fb5';
 $(function () {
-    //Console.log(query);
-    setInterval(MyGetTime, 1000);
-    //document.getElementById("time").innerHTML = turnTime.toString();
-    //get size as parameter from configuration 
-    document.getElementById("board1").innerHTML = CreateBoard(2);
-    remainingCards = data.numOfCards;
-        
+    //assume we've got data object from GET
+    var data = {
+        overallTime: "",// times in milliseconds
+        personalTime: 5000,
+        numOfCards: 2,
+        numOfAgents: 4
+    };
+    
+    var agents = [function () { console.log("player"); }, function () { console.log("agent1"); },
+    function () { console.log("agent2"); }, function () { console.log("agent3"); }]
+
+    noa = data.numOfAgents;
+    //sets the global time of the game.
+    setInterval(function () {
+        globalTime.setSeconds(globalTime.getSeconds() + 1);
+        document.getElementById("time").innerHTML = globalTime.toLocaleTimeString();
+    }, 1000);
+
+    //set interval to change turns between players
+    
+    setInterval(function () {
+        if (currentPlayer == noa - 1) {
+            currentPlayer = 0;
+            lockClicks = true
+        } else {
+            currentPlayer += 1;
+        }
+        agents[currentPlayer]();
+    }, data.personalTime)
+
+    //Initilize baord
+    document.getElementById("board1").innerHTML = CreateBoard(data.numOfCards);
+    remainingCards = data.numOfCards * data.numOfCards;
+
     function IsPair(choicesIndexes) {
         if (cardsNames[choicesIndexes[0][0]][choicesIndexes[0][1]] == cardsNames[choicesIndexes[1][0]][choicesIndexes[1][1]]) {
             var table = $("#board")[0];
@@ -35,9 +60,12 @@ $(function () {
             remainingCards -= 2;
         }
     }
-
+  
     $("button").click(async function () {
-        
+        console.log(currentPlayer);
+        if (currentPlayer % noa != 0 || lockClicks) {
+            return;
+        }
         parent = $(this).parent();
         var p_row = parent.attr("ws-Row");
         var p_col = parent.attr("ws-Column");
@@ -49,13 +77,14 @@ $(function () {
         img.width = 115;
         img.height = 115;
         
+        //$(this).css("background-color", "yellow")
+        //var imageUrl = cards[parseInt(p_row)][parseInt(p_col)];
+        //$(this).css({ "background-image": "url(" + imageUrl + ")", "background - position": "center", "background - repeat": "no - repeat", "background - size": "auto" });
+        $(this).append(img);
 
-        $(this).append(img); 
-        
-        
         await sleep(3000);
         $(img).fadeOut();
-
+        $(this).css("background-color", "cadetblue")
         if (firstChoise) {
             firstChoise = false;
             choicesIndexes[0] = [p_row, p_col];
@@ -63,11 +92,15 @@ $(function () {
             firstChoise = true;
             choicesIndexes[1] = [p_row, p_col]
             IsPair(choicesIndexes);
-            turns.concat(choicesIndexes);$()
+            turns.concat(choicesIndexes);//$()
+            
             if (remainingCards == 0) {
                 document.getElementById("board1").innerHTML = "<h1>game over</h1>";
             }
         }
+        firstChoise = true;
+        choicesIndexes = {};
+        lockClicks = true;
     });
    
 });
@@ -90,6 +123,16 @@ function CreateBoard(size) {
     return tableTag;
 }
 
+
+
+
+
+
+
+
+
+
+
 function checkTime(i) {
     if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
     return i;
@@ -97,14 +140,14 @@ function checkTime(i) {
 
 //get current UTC Time
 function MyGetTime() {
-    let now = new Date(0);
+    let now = new Date();
     let year = now.getUTCFullYear();
     let month = checkTime(now.getUTCMonth() + 1);
     let day = checkTime(now.getUTCDate());
     let hour = checkTime(now.getUTCHours());
     let minute = checkTime(now.getUTCMinutes());
     let second = checkTime(now.getUTCSeconds());
-    document.getElementById("time").innerHTML =  year + '-' + month + '-' + day + 'T' + hour + ':' + minute + ':' + second + 'Z';
+    return year + '-' + month + '-' + day + 'T' + hour + ':' + minute + ':' + second + 'Z';
 }
 
 
